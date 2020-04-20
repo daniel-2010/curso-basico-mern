47db0383d845c3ba52bcc09add6d4031975f8cf1
Daniel C <daniel-2.010@hotmail.com>
2020-04-20 16:44:43 -0300

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>CURSO BÁSICO DE NODEJS COM REACTJS</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
